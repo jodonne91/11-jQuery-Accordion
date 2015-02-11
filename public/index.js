d8d1dd94b653a4b3a33658939d0c719e98f32f86
jodonne91 <jodonne91@gmail.com>
2015-02-10 21:04:09 -0500

    added code

--- public/index.js
+++ public/index.js
@@ -1,1 +1,44 @@
 //index.js
+
+$(document).ready(function() {
+	$('.cardBody').addClass("hidden");
+
+    $('#firstHeading').on("click", function(){
+    	$('.cardBody').addClass("hidden");
+    	$('#firstBody').removeClass("hidden");
+    });
+
+    $('#secondHeading').on("click", function(){
+    	$('.cardBody').addClass("hidden");
+    	$('#secondBody').removeClass("hidden");
+    });
+
+    $('#thirdHeading').on("click", function(){
+    	$('.cardBody').addClass("hidden");
+    	$('#thirdBody').removeClass("hidden");
+    });
+
+});
+
+
+
+
+
+
+//  var position1 = {
+// 	$('#Section_one').css('top: 10px')
+// 	$('Section_two')
+// 	$('Section_three')
+// }
+
+// #Section_one {
+// 	top: 0px
+// }
+
+// #Section_two {
+// 	top: 42px;
+// }
+
+// #Section_three {	
+// 	top: 84px;
+// }
